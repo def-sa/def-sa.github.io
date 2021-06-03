06666dc60f970ea70f58cab36fc16f07d7d2a758
defsa <69076081+def-sa@users.noreply.github.com>
2021-06-03 06:49:41 -0500

    testing random splash

--- script.js
+++ script.js
@@ -71,3 +71,16 @@
     }
     return "";
   }
+  
+  function splash() {
+	fetch('splashes.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    var random = Math.floor(Math.random() * (data.length));
+    document.getElementById('splash').innerHTML = data[random];
+  });
+	  
+  }
+  
