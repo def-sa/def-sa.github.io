e169f9e3a49f6601a4470d0c70cf67339df88542
defsa <69076081+def-sa@users.noreply.github.com>
2021-06-06 10:24:05 -0500

    img invert

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 
-// load setting
+function loadSetting() {
 var cookie = getCookie("darkmode");
 var cookieparse = (cookie === 'true');
 if (cookie !== '' || cookie !== null) { //if cookie is defined
 	if (cookieparse == true) {
 		document.body.style.backgroundColor = "#3c3c3c";
         document.body.style.filter = "invert(1)";
-        for (var i = 0; i < document.getElementsByTagName('img').length; i++) {
-          document.getElementsByTagName('img')[i].style.filter = "invert(1)";
-        }
+        imginvert(1);
         document.getElementById("darkmode").innerHTML = "lighten";
       }
       if (cookieparse == false) {
         document.body.style.backgroundColor = "#c3c3c3";
         document.body.style.filter = "invert(0)";
-        imgfix();
+        imginvert(0);
         document.getElementById("darkmode").innerHTML = "darken";
       }
     } else { //if cookie is not defined
       setCookie("darkmode", false, 30);
       }
-	  
+}
+
 function darkmode() {
     if (getCookie("darkmode") == 'true') {
       dark = true;
@@ -28,16 +27,14 @@ function darkmode() {
     if (typeof dark == 'undefined') { //if loop or first time
       document.body.style.backgroundColor = "#3c3c3c";
       document.body.style.filter = "invert(1)";
-      for (var i = 0; i < document.getElementsByTagName('img').length; i++) {
-        document.getElementsByTagName('img')[i].style.filter = "invert(1)";
-      }
+      imginvert(1);
       document.getElementById("darkmode").innerHTML = "lighten";
       dark = true;
       setCookie("darkmode", dark, 30); //set cookie to false
     } else { //if dark mode is active
       document.body.style.backgroundColor = "#c3c3c3";
       document.body.style.filter = "invert(0)";
-      imgfix();
+      imginvert(0);
       document.getElementById("darkmode").innerHTML = "darken";
       dark = undefined; //restart
       setCookie("darkmode", false, 30); //set cookie to false
@@ -67,9 +64,9 @@ function getCookie(cname) {
     return "";
   }
   
-function imgfix() {
+function imginvert(num) {
 	for (var i = 0; i < document.getElementsByTagName('img').length; i++) {
-		document.getElementsByTagName('img')[i].style.filter = "invert(0)";
+		document.getElementsByTagName('img')[i].style.filter = "invert("+num+")";
 	} 
 }
 
