3f4951925548d0c82da3c6ef55ffd00ddaae83c9
def-sa <69076081+def-sa@users.noreply.github.com>
2021-06-16 16:17:07 -0500

    gallery redesign
    and splashes update

--- gallery/script.js
+++ gallery/script.js
@@ -1,91 +1,71 @@
-var t = {};
-
-fetch('./art.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    for (var [key, value] of Object.entries(data)) {
-      value = `${value}`.split(","); //make array of links
-      switch(`${key}`) {
-      case "ideas":
-        t.ideasvalue = value;
-        createImg(`${key}`,value);
-        for (i = 0; i < value.length; i++) { //for each link
-          document.getElementById(`${key}`+"img"+i).style.backgroundColor = "black";
-		  document.getElementById(`${key}`+"img"+i).style.color = "white";
-        }
-        break;
-      case "drawings":
-        t.drawingsvalue = value;
-        createImg(`${key}`,value);
-        for (i = 0; i < value.length; i++) {
-          document.getElementById(`${key}`+"img"+i).style.backgroundColor = "gray";
-		  document.getElementById(`${key}`+"img"+i).style.color = "white";
-        }
-        break;
-      case "edits":
-        t.editsvalue = value;
-        createImg(`${key}`,value);
-        for (i = 0; i < value.length; i++) {
-          document.getElementById(`${key}`+"img"+i).style.backgroundColor = "lightgray";
-		  document.getElementById(`${key}`+"img"+i).style.color = "black";
-        }
-        break;
-	  case "other":
-        t.othervalue = value;
-        createImg(`${key}`,value);
-        for (i = 0; i < value.length; i++) {
-          document.getElementById(`${key}`+"img"+i).style.backgroundColor = "white";
-		  document.getElementById(`${key}`+"img"+i).style.color = "black";
-        }
-        break;
+  fetch('art.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      for (const [key, value] of Object.entries(data)) {
+        createCheckbox(key);
+        createImg(key, value);
       }
-    }
-  });
-  
+    });
+    
 function createImg(key,value) {
-  document.getElementById(key).checked = true;
+  //create bg
+  var bg = document.createElement('div');
+  bg.style.position = "inline";
+  bg.id = key+"bg";
+  switch (key) { //customize specific bg
+    case "ideas":
+      bg.style.border = "3px dotted black";
+      document.getElementById(key).checked = false;
+    break;
+    case "edits":
+      bg.style.display = "none";
+	  document.getElementById(key).checked = false;
+    break;
+	case "other":
+      bg.style.display = "none";
+	  document.getElementById(key).checked = false;
+    break;
+	case "animated":
+      bg.style.display = "none";
+	  document.getElementById(key).checked = false;
+    break;
+    default: //if no case is met, mark as checked
+      document.getElementById(key).checked = true;
+    break;
+    }
   for (i = 0; i < value.length; i++) { //for each link
-    var img = document.createElement('img');
-    img.id = key+"img"+i;
+    bg.style.backgroundColor = "rgba(0, 0, 0, 0."+i+"5)";
+    //create image
+    var img = document.createElement('img'); 
+    img.id = key+i;
     img.src = value[i];
     img.alt = key;
-	img.loading = "lazy";
+    //create anchor
     var a = document.createElement("a");
-    a.setAttribute("href", value[i]);
-	a.setAttribute('target', '_blank');
+    a.href = value[i];
     a.appendChild(img);
-    document.getElementById('art').appendChild(a);
+    bg.appendChild(a);
+    document.getElementById('art').appendChild(bg);
+    
   }
 }
 
-function toggle(obj) {
-  switch(obj.id) {
-    case "ideas":
-      for (i = 0; i < t.ideasvalue.length; i++) {
-        toggle_vis("ideasimg"+i);
-      }
-      break;
-    case "drawings":
-      for (i = 0; i < t.drawingsvalue.length; i++) {
-        toggle_vis("drawingsimg"+i);
-      }
-      break;
-    case "edits":
-      for (i = 0; i < t.editsvalue.length; i++) {
-        toggle_vis("editsimg"+i);
-      }
-      break;
-	case "other":
-      for (i = 0; i < t.othervalue.length; i++) {
-        toggle_vis("otherimg"+i);
-      }
-      break;
-    }
-}
+function createCheckbox(key) {
+  var check = document.createElement('input');
+  check.id = key;
+  check.type = "checkbox";
+  check.name = key;
+  check.setAttribute("onchange" , "toggle(this)");
+  var label = document.createElement('label');
+  label.for = "input";
+  label.innerHTML = key+":";
+  document.getElementById('input').appendChild(label);
+  document.getElementById('input').appendChild(check);
+  }
 
-function toggle_vis(id) {
-  var e = document.getElementById(id);
-  e.style.display = ((e.style.display!='none') ? 'none' : 'inline');
+function toggle(obj) {
+    var e = document.getElementById(obj.id+"bg"); //if all images under key value
+    e.style.display = ((e.style.display!='none') ? 'none' : 'inherit');
 }
